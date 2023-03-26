ec1b0e0adcf5b2e183c4761d9ac5ad66c870a9b5
donnik007 <56132989+donnik007@users.noreply.github.com>
2023-03-26 21:33:45 +0200

    Update app.js

--- js/app.js
+++ js/app.js
@@ -3,3 +3,30 @@ console.log('Hello World - działam!');
 function vibrate() {
   navigator.vibrate([500, 100, 500, 100, 500, 100, 1000, 100, 1000, 100, 1000, 100,500, 100, 500, 100, 500, 100])
 }
+
+function lokalizacja() {
+  navigator.geolocation.getCurrentPosition(position => {
+    const { latitude, longitude } = position.coords;
+
+    // Creating map options
+    var x = position.coords.latitude
+    var y = position.coords.longitude
+    var mapOptions = {
+        center: [x, y],
+        zoom: 10
+    }
+    var map = new L.map('map', mapOptions);
+    var layer = new L.TileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png');
+    map.addLayer(layer);
+
+    // Creating a Marker
+    var markerOptions = {
+        title: "MyLocation",
+        clickable: true,
+        draggable: true
+    }
+    var marker = L.marker([x,y], markerOptions);
+    marker.addTo(map);
+
+  });  
+}
