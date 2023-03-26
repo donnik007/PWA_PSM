c30aa55fe2f8e382aadbdd1f0b37fecd5ba3ea46
donnik007 <56132989+donnik007@users.noreply.github.com>
2023-03-26 19:26:48 +0200

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,6 +1,10 @@
 console.log('Hello World - działam!');
 
-
-document.getElementById('sos_button').addEventListener('click', function() {
-            navigator.vibrate([300, 250, 300, 250, 300, 250, 1000, 250, 1000, 250, 1000, 250, 300, 250, 300, 250, 300]);
-          }
+if(navigator.vibrate) {
+        function vibrate() {
+            navigator.vibrate([300, 250, 300, 250, 300, 250, 1000, 250, 1000, 250, 1000, 250, 300, 250, 300, 250, 300])
+        }
+    } else {
+        error.innerHTML = "Vibrate API not supported in this device."
+        error.classList.remove("close")
+    }
