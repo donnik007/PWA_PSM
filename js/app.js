284c0625d1d5ab269ff4321cd08c0101ef9339c9
donnik007 <56132989+donnik007@users.noreply.github.com>
2023-03-26 19:36:10 +0200

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,6 +1,8 @@
 console.log('Hello World - działam!');
 
 function vibrate() {
+        console.log('A');
         navigator.vibrate([300, 250, 300, 250, 300, 250, 1000, 250, 1000, 250, 1000, 250, 300, 250, 300, 250, 300])
+        console.log('B');
 }
 
